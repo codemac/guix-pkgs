aeaec4a7bfa181d7f11c7369ca1dd6c92e9fb23a
Jeff Mickey <j@codemac.net>
2015-07-29 14:19:24 -0700

    First attempt at a go package
    Signed-off-by: Jeff Mickey <j@codemac.net>

---
+++ codemac/packages/go.scm
@@ -0,0 +1,151 @@
+(define-module (codemac packages go)
+  #:use-module (guix download)
+  #:use-module (guix build-system trivial)
+  #:use-module (guix git-download)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages elf)
+  #:use-module (gnu packages bash))
+
+(define-public go
+  (package
+    (name "go")
+    (version "1.4.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://storage.googleapis.com/golang/" name version  ".src.tar.gz"))
+       (sha256
+        (base32 "16sygfgdf7krv2vicnyx4j7awlmjwwkfkg86phawxzddz3c6z6i9"))))
+    (build-system trivial-build-system)
+    (arguments
+     `(#:modules ((guix build utils))
+                 #:builder
+                 (begin
+                   (use-modules (guix build utils))
+                   (use-modules (srfi srfi-1))
+                   (use-modules (ice-9 ftw))
+                   (let* ((tarballgz (assoc-ref %build-inputs "source"))
+                          (out-usrgo (string-append (assoc-ref %outputs "out") "/usr/lib/go"))
+                          (out-bin (string-append (assoc-ref %outputs "out") "/bin"))
+                          (out-doc (string-append (assoc-ref %outputs "out") "/usr/share/doc"))
+                          (tar (string-append (assoc-ref %build-inputs "tar") "/bin"))
+                          (gzip (string-append (assoc-ref %build-inputs "gzip") "/bin"))
+                          (bash (string-append (assoc-ref %build-inputs "bash") "/bin"))
+                          (cu (string-append (assoc-ref %build-inputs "coreutils") "/bin"))
+                          (gcc (string-append (assoc-ref %build-inputs "gcc") "/bin"))
+                          (gcclib (string-append (assoc-ref %build-inputs "gcc") "/lib"))
+                          (grep (string-append (assoc-ref %build-inputs "grep") "/bin"))
+                          (lh (string-append (assoc-ref %build-inputs "lheaders") "/include"))
+                          (glibclib (string-append (assoc-ref %build-inputs "glibc") "/lib"))
+                          (bu (string-append (assoc-ref %build-inputs "binutils") "/bin"))
+                          (iana (string-append (assoc-ref %build-inputs "iana") "/etc"))
+                          (sed (string-append (assoc-ref %build-inputs "sed") "/bin/sed"))
+                          (patchelf (string-append (assoc-ref %build-inputs "patchelf") "/bin/patchelf"))
+                          (tz (string-append (assoc-ref %build-inputs "tzdata") "/share/zoneinfo")))
+                     (begin
+                       (setenv "PATH" (fold (lambda (x y) (string-append y ":" x)) "."
+                                            (list tar gzip bash cu gcc grep bu)))
+                       (zero? (system* "tar" "xvf" tarballgz))
+                       (chdir "go/src")
+                       ;;   # Disabling the 'os/http/net' tests (they want files not available in
+                       ;;   # chroot builds)
+                       ;;   rm src/net/{multicast_test.go,parse_test.go,port_test.go}
+
+                       (for-each delete-file '("net/multicast_test.go"
+                                               "net/parse_test.go"
+                                               "net/port_test.go"))
+                       
+                       ;;   # The os test wants to read files in an existing path. Just don't let it be /usr/bin.
+                       ;;   sed -i 's,/usr/bin,'"`pwd`", src/os/os_test.go
+                       ;;   sed -i 's,/bin/pwd,'"`type -P pwd`", src/os/os_test.go
+                       
+                       (substitute* "os/os_test.go"
+                         (("/bin/pwd") (which "pwd"))
+                         (("/usr/bin") (dirname (which "pwd")))
+                         (("TestHostname") "areturn"))
+
+                       ;;   # Disable the unix socket test
+                       ;;   sed -i '/TestShutdownUnix/areturn' src/net/net_test.go
+
+                       (substitute* "net/net_test.go"
+                         (("TestShutdownUnix") "areturn"))
+
+                       ;;   # Disable the hostname test
+                       ;;   sed -i '/TestHostname/areturn' src/os/os_test.go
+                       ;;   sed -i 's,/etc/protocols,${iana_etc}/etc/protocols,' src/net/lookup_unix.go
+
+                       (substitute* "net/lookup_unix.go"
+                         (("/etc/protocols") (string-append iana "protocols")))
+
+                       ;;   # ParseInLocation fails the test
+                       ;;   sed -i '/TestParseInSydney/areturn' src/time/format_test.go
+                       (substitute* "time/format_test.go"
+                         (("TestParseInSydney") "areturn"))
+
+                       ;; '' + lib.optionalString stdenv.isLinux ''
+                       ;;   sed -i 's,/usr/share/zoneinfo/,${tzdata}/share/zoneinfo/,' src/time/zoneinfo_unix.go
+                       (substitute* "time/zoneinfo_unix.go"
+                         (("/usr/share/zoneinfo/") tz))
+
+                       ;;   sed -i 's,/lib/ld-linux.so.3,${loaderArm},' src/cmd/5l/asm.c
+                       ;;   sed -i 's,/lib64/ld-linux-x86-64.so.2,${loaderAmd64},' src/cmd/6l/asm.c
+                       ;; (substitute* "cmd/6l/asm.c"
+                       ;;   (("/lib64/ld-linux-x86-64.so.2") (string-append glibclib "/ld-linux-x86-64.so.2")))
+                       ;;   sed -i 's,/lib/ld-linux.so.2,${loader386},' src/cmd/8l/asm.c
+
+                       ;; patch the go tools rpath before running run.bash. Will repatch everything at the end
+
+                       ;;
+                       ;; (string-append "GO_LDFLAGS=-r '" gcclib ":" glibclib "'")
+                       ;; (string-append "GO_LDFLAGS=-extldflags -static")
+                       ;; (string-append "GO_LDFLAGS=-extldflags '-Wl,-rpath," gcclib " -Wl,-rpath," glibclib "'")
+                       (zero? (system* "sed" "-i" (string-append "13i" patchelf " --set-interpreter" (string-append glibclib "/ld-linux-x86-64.so.2") "../bin/go") "./all.bash"))
+                       (zero? (system* "sed" "-i" (string-append "13i" patchelf " --set-rpath" (string-append gcclib ":" glibclib) "../bin/go") "./all.bash"))
+
+                       (zero? (system* "env"
+                                       (string-append "GOROOT_FINAL=" out-usrgo)
+                                       (string-append "CPATH=" lh)
+                                       (string-append "LIBRARY_PATH=" glibclib ":" gcclib)
+                                       (string-append "LD_LIBRARY_PATH=" gcclib)
+                                       "bash" "./all.bash"))
+                       (chdir "../..")
+                       (for-each mkdir-p (list out-usrgo out-bin out-doc))
+                       (for-each (lambda (x) (copy-recursively (string-append "go/" x) out-usrgo))
+                                 (filter (lambda (x) (not (or (equal? x "bin") (equal? x "doc"))))
+                                         (scandir "go")))
+                       (copy-recursively "go/bin" out-bin)
+                       (copy-recursively "go/doc" out-doc)
+                       (symlink out-bin (string-append out-usrgo "/bin"))
+                       (symlink out-doc (string-append out-usrgo "/doc")))))
+                 (native-inputs `(("tar" ,tar)
+                                  ("gzip" ,gzip)
+                                  ("bash" ,bash)
+                                  ("coreutils" ,coreutils)
+                                  ("grep" ,grep)
+                                  ("gcc" ,gcc)
+                                  ("binutils" ,binutils)
+                                  ("glibc" ,glibc)
+                                  ("lheaders" ,linux-libre-headers)
+                                  ("sed" ,sed)
+                                  ("patchelf" ,patchelf-0.8.git)))))
+    (inputs `(("glibc" ,glibc)
+              ("gcc-lib" ,gcc "lib")
+              ("iana" ,net-base)
+              ("tzdata" ,tzdata)))
+    (home-page "http://golang.org")
+    (synopsis "Go is an open source programming language that makes it easy to
+build simple, reliable, and efficient software.")
+    (description "Go is expressive, concise, clean, and efficient. Its
+concurrency mechanisms make it easy to write programs that get the most out of
+multicore and networked machines, while its novel type system enables flexible
+and modular program construction. Go compiles quickly to machine code yet has
+the convenience of garbage collection and the power of run-time
+reflection. It's a fast, statically typed, compiled language that feels like a
+dynamically typed, interpreted language.")
+  (license bsd-3)))
---
+++ codemac/packages/patchelf.scm
@@ -0,0 +1,50 @@
+(define-module (codemac packages patchelf)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module ((guix licenses) #:select (gpl3+ lgpl3+ lgpl2.0+))
+  #:use-module (gnu packages)
+  #:use-module (gnu packages m4)
+  #:use-module (gnu packages compression))
+
+(define-public patchelf
+  (package
+    (name "patchelf")
+    (version "0.8.git")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference (url "git://github.com/NixOS/patchelf.git")
+                                  (commit "b202ad239ed815fbc59dd0a5cb2def5991116c42")))
+             (sha256
+              (base32
+               "1rqpg84wrd3fa16wa9vqdvasnc05yz49w207cz1l0wrl4k8q97y9"))
+             (patches (list (search-patch "patchelf-page-size.patch")))))
+    (build-system gnu-build-system)
+
+    ;; XXX: The upstream 'patchelf' doesn't support ARM.  The only available
+    ;;      patch makes significant changes to the algorithm, possibly
+    ;;      introducing bugs.  So, we apply the patch only on ARM systems.
+    (inputs
+     (if (string-prefix? "arm" (or (%current-target-system) (%current-system)))
+         `(("patch/rework-for-arm" ,(search-patch
+                                     "patchelf-rework-for-arm.patch")))
+         '()))
+    (arguments
+     (if (string-prefix? "arm" (or (%current-target-system) (%current-system)))
+         `(#:phases (alist-cons-after
+                     'unpack 'patch/rework-for-arm
+                     (lambda* (#:key inputs #:allow-other-keys)
+                       (let ((patch-file
+                              (assoc-ref inputs "patch/rework-for-arm")))
+                         (zero? (system* "patch" "--force" "-p1"
+                                         "--input" patch-file))))
+                     %standard-phases))
+         '()))
+
+    (home-page "http://nixos.org/patchelf.html")
+    (synopsis "Modify the dynamic linker and RPATH of ELF executables")
+    (description
+     "PatchELF allows the ELF \"interpreter\" and RPATH of an ELF binary to be
+changed.")
+    (license gpl3+)))

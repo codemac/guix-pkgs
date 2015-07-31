2643c40757f1f5092ff7823b5e02705e26c04d30
Jeff Mickey <j@codemac.net>
2015-07-31 13:47:48 -0700

    go builds!

--- codemac/packages/go.scm
+++ codemac/packages/go.scm
@@ -4,6 +4,7 @@
   #:use-module (guix git-download)
   #:use-module (guix licenses)
   #:use-module (guix packages)
+  #:use-module (gnu packages commencement)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages base)
   #:use-module (gnu packages gcc)
@@ -37,7 +38,7 @@
                           (bash (string-append (assoc-ref %build-inputs "bash") "/bin"))
                           (cu (string-append (assoc-ref %build-inputs "coreutils") "/bin"))
                           (gcc (string-append (assoc-ref %build-inputs "gcc") "/bin"))
-                          (gcclib (string-append (assoc-ref %build-inputs "gcc") "/lib"))
+                          (gcclib (string-append (assoc-ref %build-inputs "gcc-lib") "/lib"))
                           (grep (string-append (assoc-ref %build-inputs "grep") "/bin"))
                           (lh (string-append (assoc-ref %build-inputs "lheaders") "/include"))
                           (glibclib (string-append (assoc-ref %build-inputs "glibc") "/lib"))
@@ -48,68 +49,42 @@
                      (begin
                        (setenv "PATH" (fold (lambda (x y) (string-append y ":" x)) "."
                                             (list tar gzip bash cu gcc grep bu)))
+                       (setenv "LD_LIBRARY_PATH" (string-append gcclib ":" glibclib))
                        (zero? (system* "tar" "xvf" tarballgz))
                        (chdir "go/src")
                        ;;   # Disabling the 'os/http/net' tests (they want files not available in
                        ;;   # chroot builds)
-                       ;;   rm src/net/{multicast_test.go,parse_test.go,port_test.go}
-
                        (for-each delete-file '("net/multicast_test.go"
                                                "net/parse_test.go"
                                                "net/port_test.go"))
                        
                        ;;   # The os test wants to read files in an existing path. Just don't let it be /usr/bin.
-                       ;;   sed -i 's,/usr/bin,'"`pwd`", src/os/os_test.go
-                       ;;   sed -i 's,/bin/pwd,'"`type -P pwd`", src/os/os_test.go
-                       
                        (substitute* "os/os_test.go"
                          (("/bin/pwd") (which "pwd"))
                          (("/usr/bin") (dirname (which "pwd")))
                          (("TestHostname") "areturn"))
 
                        ;;   # Disable the unix socket test
-                       ;;   sed -i '/TestShutdownUnix/areturn' src/net/net_test.go
-
                        (substitute* "net/net_test.go"
                          (("TestShutdownUnix") "areturn"))
 
                        ;;   # Disable the hostname test
-                       ;;   sed -i '/TestHostname/areturn' src/os/os_test.go
-                       ;;   sed -i 's,/etc/protocols,${iana_etc}/etc/protocols,' src/net/lookup_unix.go
-
                        (substitute* "net/lookup_unix.go"
                          (("/etc/protocols") (string-append iana "protocols")))
 
                        ;;   # ParseInLocation fails the test
-                       ;;   sed -i '/TestParseInSydney/areturn' src/time/format_test.go
                        (substitute* "time/format_test.go"
                          (("TestParseInSydney") "areturn"))
 
-                       ;; '' + lib.optionalString stdenv.isLinux ''
-                       ;;   sed -i 's,/usr/share/zoneinfo/,${tzdata}/share/zoneinfo/,' src/time/zoneinfo_unix.go
                        (substitute* "time/zoneinfo_unix.go"
                          (("/usr/share/zoneinfo/") tz))
 
-                       ;;   sed -i 's,/lib/ld-linux.so.3,${loaderArm},' src/cmd/5l/asm.c
-                       ;;   sed -i 's,/lib64/ld-linux-x86-64.so.2,${loaderAmd64},' src/cmd/6l/asm.c
-                       ;; (substitute* "cmd/6l/asm.c"
-                       ;;   (("/lib64/ld-linux-x86-64.so.2") (string-append glibclib "/ld-linux-x86-64.so.2")))
-                       ;;   sed -i 's,/lib/ld-linux.so.2,${loader386},' src/cmd/8l/asm.c
-
-                       ;; patch the go tools rpath before running run.bash. Will repatch everything at the end
-
-                       ;;
-                       ;; (string-append "GO_LDFLAGS=-r '" gcclib ":" glibclib "'")
-                       ;; (string-append "GO_LDFLAGS=-extldflags -static")
-                       
-                       ;; (zero? (system* sed "-i"
-                       ;;                 (string-append "13i" patchelf " --set-interpreter " (string-append glibclib "/ld-linux-x86-64.so.2") " ../bin/go") "./all.bash"))
-                       ;; (zero? (system* sed "-i" (string-append "13i" patchelf " --set-rpath " (string-append gcclib ":" glibclib) " ../bin/go") "./all.bash"))
+                       (substitute* "cmd/6l/asm.c" (("/lib64/ld-linux-x86-64.so.2") (string-append glibclib "/ld-linux-x86-64.so.2")))
                        (zero? (system* sed "-i" "3iset -x" "./make.bash"))
                        (zero? (system* sed "-i" "3iset -x" "./run.bash"))
                        (zero? (system* sed "-i" "3iset -x" "./all.bash"))
                        (zero? (system* "env"
-                                       (string-append "GO_LDFLAGS=-linkmode external -extldflags \"-Wl,-rpath," gcclib " -Wl,-rpath," glibclib "\"")
+                                       (string-append "GO_LDFLAGS=-r \""gcclib ":" glibclib "\" -extldflags \"-Wl,-rpath," gcclib " -Wl,-rpath," glibclib "\"")
                                        (string-append "GOROOT_FINAL=" out-usrgo)
                                        (string-append "CPATH=" lh)
                                        (string-append "LIBRARY_PATH=" glibclib ":" gcclib)
@@ -129,13 +104,16 @@
                      ("coreutils" ,coreutils)
                      ("grep" ,grep)
                      ("gcc" ,gcc)
+                     ("gcc-lib" ,gcc "lib")
                      ("binutils" ,binutils)
                      ("glibc" ,glibc)
                      ("lheaders" ,linux-libre-headers)
                      ("sed" ,sed)))
     (inputs `(("glibc" ,glibc)
               ("gcc-lib" ,gcc "lib")
+              ("gcc" ,gcc)
               ("iana" ,net-base)
+              ("ld-wrapper" ,ld-wrapper)
               ("tzdata" ,tzdata)))
     (home-page "http://golang.org")
     (synopsis "Go is an open source programming language that makes it easy to
